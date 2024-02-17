daf45c1c1035d32c395945ea34713fda2e7707c0
Biresh <workinguse5@gmail.com>
2024-02-18 02:05:46 +0530

    quest template

--- src/components/tree.jsx
+++ src/components/tree.jsx
@@ -1,6 +1,7 @@
 import { document } from 'postcss';
 import React from 'react';
 import Tree from 'react-d3-tree';
+import { useState } from 'react';
 
 // data will be fetched from the backend
 const data = {
@@ -15,7 +16,7 @@ const data = {
         {
           name: 'Html',
           attributes: {
-            progress: "80%",
+            progress: "100%",
           },
           children: [
             {
@@ -26,7 +27,7 @@ const data = {
         {
           name: 'CSS',
           attributes: {
-            progress: "70%",
+            progress: "100%",
           },
           children: [
             {
@@ -44,6 +45,12 @@ export default function Branching() {
   // keep track of the current node
   // const [current, setCurrent] = useState("");
 
+  const [selectedNode, setSelectedNode] = useState(false);
+
+  const handleOnClick = () => {
+    setSelectedNode(true);
+  };
+
   return (
     <div id="treeWrapper" style={{ width: '100vw', height: '100vh', position: 'absolute', top: 0, left: 0 }}>
       <Tree 
@@ -51,12 +58,29 @@ export default function Branching() {
         orientation='vertical' 
         zoomable={false} 
         draggable={false} 
+        collapsible={false}
         translate={{ x: window.innerWidth / 2, y: 50 }} // Adjust y value as needed
         translateExtent={[
           [0, 0],
           [window.innerWidth, window.innerHeight]
         ]}
+        onNodeClick={handleOnClick}
       />
+      {selectedNode && (
+            <div className="fixed z-10 inset-0 overflow-y-auto" onClick={() => setSelectedNode(false)}>
+                <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                    <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+                        <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+                    </div>
+
+                    <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+
+                    <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" onClick={(e) => e.stopPropagation()}>
+                        <h1>Node</h1>
+                    </div>
+                </div>
+            </div>
+        )}
     </div>
   );
 }
